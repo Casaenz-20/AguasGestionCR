98c444c03ac319ab8ccc0cebdcd24f4ab23e6a07
Meylin Dominguez <meylind470@gmail.com>
2026-07-28 13:10:07 -0600

    Add Clientes and ReportView WPF windows and interfaces
    Added Clientes.xaml with modern UI for client registration/editing and basic event handlers in Clientes.xaml.cs. Introduced empty IClientes interface. Added ReportView.xaml.cs with window class and stub event handlers.

---
+++ AguasGestionCR/ReportView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AguasGestionCR
+{
+    /// <summary>
+    /// Lógica de interacción para ReportView.xaml
+    /// </summary>
+    public partial class ReportView : Window
+    {
+        public ReportView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
