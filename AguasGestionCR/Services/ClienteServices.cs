7deaf6205a31c1146fa4b212d2191854736d03ff
Meylin Dominguez <meylind470@gmail.com>
2026-07-29 11:00:49 -0600

    Clase

---
+++ AguasGestionCR/Services/ClienteServices.cs
@@ -0,0 +1,39 @@
+﻿using AguasGestionCR.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AguasGestionCR.Services
+{
+    internal class ClienteServices //: IClientes
+    {
+        //public int RegistrarCliente(Clientes cliente)
+        //{
+        //    using (var db = newAcueductoDbContext())
+        //    {
+        //        var conn = db.Database.GetDbConnection();
+        //        using (var cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = "RegistrarCliente";
+        //            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+        //        }
+
+        //    }
+
+
+        //    }
+        public Clientes ObtenerClientePorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public bool EditarCliente(Clientes cliente)
+        {
+            throw new NotImplementedException();
+        }
+        public string EliminarCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
