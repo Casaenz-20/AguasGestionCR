dcf39300e49c0cb17a2eec3ae01ba59de89aa15c
Est_Lab01 <Est_Lab01@URHNCFPULAB0102>
2026-07-29 13:57:34 -0600

    Refactoriza envío de correos y separa lógica SMTP
    Se eliminó la lógica de envío de correos desde ReportView.xaml.cs y se trasladó a la nueva clase EviarReporte en AguasGestionCR.Services. Esta clase encapsula la configuración SMTP y el método público para enviar reportes de averías por correo electrónico. Se mejora la organización del código separando la lógica de presentación de la lógica de envío, siguiendo buenas prácticas de diseño.

---
+++ AguasGestionCR/Services/EviarReporte.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AguasGestionCR.Services
+{
+    public  class EviarReporte
+    {
+        private readonly string _correoEmpresa = "aguagestioncr@gmail.com";
+        private readonly string _contrasenaApp = "gzzracochvyksoqe";
+        private readonly string _hostSmtp = "smtp.gmail.com";
+        private readonly int _puertoSmtp = 587;
+        public void EnviarReporteAveria(
+             string correoUsuario,
+             string descripcion,
+             string medidor,
+             string tipoAveria,
+             string sector,
+             string direccion)
+        {
+            MailMessage mail = new MailMessage();
+
+            mail.To.Add(new MailAddress(_correoEmpresa));
+
+
+            mail.From = new MailAddress(correoUsuario);
+
+            mail.ReplyToList.Add(new MailAddress(correoUsuario));
+
+            mail.Subject = "Reporte de Avería de Cliente";
+            mail.Body = $"Correo de contacto del cliente: {correoUsuario}\n\n" +
+                        $"Descripción: {descripcion}\n" +
+                        $"Número de medidor: {medidor}\n" +
+                        $"Tipo de Avería: {tipoAveria}\n" +
+                        $"Sector / Comunidad: {sector}\n" +
+                        $"Dirección Exacta: {direccion}";
+
+            using (SmtpClient smtp = new SmtpClient(_hostSmtp, _puertoSmtp))
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(_correoEmpresa, _contrasenaApp);
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+            }
+        }
+
+    }
+    }
+
