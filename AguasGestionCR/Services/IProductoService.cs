b105b587ba6b4643f062d255d31ff2e4710e2aa0
Cisneros-Tech <cisnerosastorgak@gmail.com>
2026-07-29 11:04:06 -0600

    #

---
+++ AguasGestionCR/Services/IProductoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AguasGestionCR.Models;
+using System.Collections.Generic;
+
+namespace AguasGestionCR.Services
+{
+    public interface IProductoService
+    {
+       
+
+    }
+}
