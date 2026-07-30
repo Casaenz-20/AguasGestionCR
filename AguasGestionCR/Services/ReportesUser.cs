07003c60192916b51f55edd8ecaf63d25a886ef7
Est_Lab01 <Est_Lab01@URHNCFPULAB0102>
2026-07-30 11:56:53 -0600

    Mejora validación y envío de reportes por rol de usuario
    Se mejora la validación de campos obligatorios en ReportView.xaml.cs y se diferencia el envío de reportes según el rol ("Usuario" o "Administrador"). Se añade la clase ReportesUser para gestionar el envío de reportes por correo vía SMTP para usuarios, con mensajes de éxito o error. Para administradores, se mantiene EviarReporte con mejor manejo de errores. Se agrega el archivo ReportesUser.cs en AguasGestionCR.Services.

---
+++ AguasGestionCR/Services/ReportesUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+
+namespace AguasGestionCR.Services
+{
+    public  class ReportesUser
+    {
+        private readonly string _correoEmpresa = "aguagestioncr@gmail.com";
+        private readonly string _contrasenaApp = "gzzracochvyksoqe";
+        private readonly string _hostSmtp = "smtp.gmail.com";
+        private readonly int _puertoSmtp = 587;
+        public void EnviarReporte(
+
+             string descripcion,
+             string medidor,
+             string tipoAveria,
+             string sector,
+             string direccion,
+             string correoContacto)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(new MailAddress(correoContacto));
+
+            mail.From = new MailAddress(_correoEmpresa);
+
+            mail.ReplyToList.Add(new MailAddress(correoContacto));
+            mail.Subject = "Reporte de Avería de Cliente";
+            mail.Body = $"Correo dirigido a: {correoContacto}\n\n" +
+                        $"Descripción: {descripcion}\n" +
+                        $"Número de medidor: {medidor}\n" +
+                        $"Tipo de Avería: {tipoAveria}\n" +
+                        $"Sector / Comunidad: {sector}\n" +
+                        $"Dirección Exacta: {direccion}";
+
+
+            using (SmtpClient smtp = new SmtpClient(_hostSmtp, _puertoSmtp))
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(_correoEmpresa, _contrasenaApp);
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+            }
+        }
+    }
+}
